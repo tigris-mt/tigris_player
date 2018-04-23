db85d16c2a405a626df76742110bca847ce96e8d
Beha <shacknetisp@mail.com>
2018-04-23 10:25:59 -0400

    Add faction API.

---
+++ init.lua
@@ -0,0 +1,11 @@
+local m = {}
+tigris.player = m
+
+-- Get unique faction ID for a player.
+-- Override in faction mod.
+-- Format:
+--- player:<name> if player is not in a faction.
+--- faction:<id> if player is in a faction.
+function m.faction(name)
+    return "player:" .. name
+end
